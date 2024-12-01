4223ca3894a399a4a507468987207cf9e0b6c80a
Oliver <safstrom.oliver@gmail.com>
2024-12-01 15:12:30 +0100

    Day1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-pub fn day1(input: String) -> u32 {
+pub fn day1(input: String) -> (u32, u32) {
     let mut left = vec![];
     let mut right = vec![];
     let split_input: Vec<&str> = input.lines().collect();
@@ -15,14 +15,34 @@ pub fn day1(input: String) -> u32 {
 
     let mut sum = 0;
 
-    for (idx, val) in left.into_iter().enumerate() {
+    for (idx, val) in left.iter().enumerate() {
         let left_val: u32 = val.parse().unwrap();
         let right_val: u32 = right[idx].parse().unwrap();
 
         sum += diff(left_val, right_val);
     }
+    let mut similarity = 0;
+    left.into_iter().for_each(|left_val| {
+        let count = count_appearance(left_val, &right);
+
+        let parsed_left: u32 = left_val.parse().unwrap();
+
+        similarity += parsed_left * count
+    });
 
     dbg!(sum);
+    dbg!(similarity);
+
+    (sum, similarity)
+}
+
+fn count_appearance(left_val: &str, right: &[&str]) -> u32 {
+    let mut sum = 0;
+    right.iter().copied().for_each(|right_val| {
+        if *left_val == *right_val {
+            sum += 1
+        }
+    });
 
     sum
 }
@@ -39,15 +59,24 @@ fn diff(a: u32, b: u32) -> u32 {
 mod day1_tests {
     use std::fs;
 
-    use crate::day1::day1;
+    use crate::day1::{count_appearance, day1};
 
     use super::diff;
 
     #[test]
-    fn day1_test() {
+    fn day1_part1_test() {
         let input = fs::read_to_string("./input/day1_test").unwrap();
+        let result = day1(input);
 
-        assert_eq!(day1(input), 11)
+        assert_eq!(result.0, 11)
+    }
+
+    #[test]
+    fn day1_part2_test() {
+        let input = fs::read_to_string("./input/day1_test").unwrap();
+        let result = day1(input);
+
+        assert_eq!(result.1, 31)
     }
 
     #[test]
@@ -59,5 +88,17 @@ mod day1_tests {
         assert_eq!(poss, 2);
         assert_eq!(neg, 2);
         assert_eq!(same, 0);
+    }
+
+    #[test]
+    fn count() {
+        let one = "1";
+        let two = "2";
+        let tree = "3";
+        let right = ["1", "1", "2", "3", "3", "3"];
+
+        assert_eq!(count_appearance(one, &right), 2);
+        assert_eq!(count_appearance(two, &right), 1);
+        assert_eq!(count_appearance(tree, &right), 3);
     }
 }
